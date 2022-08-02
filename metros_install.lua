ec03397e0b17acf8533c23afe520ec0527d5dcc1
Flench04 <47568518+Fleench@users.noreply.github.com>
2022-08-01 21:32:34 -0600

    Update metros_install.lua

--- metros_install.lua
+++ metros_install.lua
@@ -1,4 +1,4 @@
 shell.setDir("./")
-shell.run("wget","https://raw.githubusercontent.com/Fleench/CC-Metros/main/metrosV0")
+shell.run("wget","https://raw.githubusercontent.com/Fleench/CC-Metros/main/metrosV0.5")
 shell.run("wget", "https://raw.githubusercontent.com/LDDestroier/CC/master/progdor2.lua")
-shell.run("progdor2","-o","metrosV0","/metros")
+shell.run("progdor2","-o","metrosV0.5","/metros")
