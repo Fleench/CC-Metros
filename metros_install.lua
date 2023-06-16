6976b8dae61c9a1263840e2a25e46bfd5a210123
Flench04 <47568518+Fleench@users.noreply.github.com>
2023-06-15 18:10:44 -0600

    Update metros_install.lua

--- metros_install.lua
+++ metros_install.lua
@@ -1,5 +1,5 @@
 print("Welcome to the metrosSHELL installer. This will completly wipe your device and then install metrosSHELL.")
-print("If you have not backed up your data or don't want to install then don't continue.")
+print("If you have not backed up your data or don't want to install metrosSHELL then don't continue.")
 print("Continue? Y/N")
 while true do -- Loop until we exit
   local event, param = os.pullEvent () -- Wait for an event
@@ -31,3 +31,4 @@ end
 shell.run("wget", "https://raw.githubusercontent.com/LDDestroier/CC/master/progdor2.lua")
 shell.run("progdor2","-o","metros","/metros")
 shell.run("wget", "https://raw.githubusercontent.com/Fleench/CC-Metros/main/startup.lua")
+shell.run("reboot")
